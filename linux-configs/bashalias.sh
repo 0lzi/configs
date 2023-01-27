8267428a804d09d1aebe5094a1b071ab44527008
oliver.kelly <oliver.kelly@thehutgroup.com>
2023-01-27 14:57:59 +0000

    update newkey alias

--- linux-configs/bashalias.sh
+++ linux-configs/bashalias.sh
@@ -11,7 +11,7 @@
 #alias bye='echo bye && sudo systemctl suspend'
 #alias cpuwatch='watch -n 1 grep MHz /proc/cpuinfo'
 #alias untar='tar -zxvf '
-#alias new2048key ='ssh-keygen -t ecdsa -b 521'
+#alias newkey ='ssh-keygen -t ecdsa -b 521'
 #alias konect ="sshpass -p $MYPASS ssh"
 #alias konectpi ="sshpass -p $PIPASS ssh"
 
@@ -22,7 +22,7 @@ echo "#Custom Alias " >> ~/.bashrc
 echo "alias bye='echo bye && sudo systemctl suspend'" >> ~/.bashrc
 echo "alias cpuwatch='watch -n 1 grep MHz /proc/cpuinfo'" >> ~/.bashrc
 echo "alias untar='tar -zxvf ' " >> ~/.bashrc
-echo "alias new2048key='ssh-keygen -t ecdsa -b 521'" >> ~/.bashrc
+echo "alias newkey='ssh-keygen -t ecdsa -b 521'" >> ~/.bashrc
 echo "alias diskuseage='sudo baobab'" >> ~/.bashrc
 echo "alias konect ="sshpass -p $MYPASS ssh"" >> ~/.bashrc
 echo "alias konectpi ="sshpass -p $PIPASS ssh"" >> ~/.bashrc
